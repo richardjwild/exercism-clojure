88388f9ce38fb76d87e94e568861f37e11acf3e8
Richard Wild <richardwild76@gmail.com>
2017-08-20 12:53:52 +0100

    word-count iteration 2

--- word-count/src/word_count.clj
+++ word-count/src/word_count.clj
@@ -1,25 +1,12 @@
 (ns word-count
   (require [clojure.string :as str]))
 
-(defn number-of-times-word-seen
-  [word word-map]
-  (if-let [n (get word-map word)]
-    (inc n) 1))
-
-(defn is-alphanumeric [c]
-  (or (Character/isLetter c) (Character/isDigit c)))
-
-(defn filter-and-normalize-characters [input]
-  (let [filtered (apply str (filter (fn [c] (is-alphanumeric c)) input))]
-    (if (= "" filtered) nil (str/lower-case filtered))))
-
-(defn count-distinct-words [word-list]
-  (if-let [word (first word-list)]
-    (let [words-counted-so-far (count-distinct-words (rest word-list))]
-      (if-let [filtered-word (filter-and-normalize-characters word)]
-        (assoc words-counted-so-far filtered-word (number-of-times-word-seen filtered-word words-counted-so-far))
-        words-counted-so-far))
-    {}))                                                      ; it all begins with an empty map
+(defn is-allowed-character [c] (or (= \space c) (Character/isLetter c) (Character/isDigit c)))
 
 (defn word-count [input-string]
-  (count-distinct-words (str/split input-string #" ")))
+  (frequencies
+    (filter not-empty
+            (str/split
+              (str/lower-case
+                (apply str (filter is-allowed-character input-string)))
+              #" "))))
