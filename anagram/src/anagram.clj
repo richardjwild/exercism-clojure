a3b55df28c7ad3850609933874aaf8752fdb0073
Richard Wild <richard.wild@e2y.co.uk>
2017-08-22 09:04:15 +0100

    anagram

---
+++ anagram/src/anagram.clj
@@ -0,0 +1,11 @@
+(ns anagram
+  (require [clojure.string :as str]))
+
+(defn are-anagrams [s1 s2]
+  (let [l1 (str/lower-case s1) l2 (str/lower-case s2)]
+    (and
+      (not= l1 l2)
+      (= (sort l1) (sort l2)))))
+
+(defn anagrams-for [word candidates]
+  (vec (remove (fn [candidate] (not (are-anagrams word candidate))) candidates)))
