8356cb7f73faa8dda2f9f3a63cf2262fc1d2bfd6
Richard Wild <richardwild76@gmail.com>
2017-09-07 18:55:53 +0100

    flatten-array iteration 2

--- flatten-array/src/flatten_array.clj
+++ flatten-array/src/flatten_array.clj
@@ -1,4 +1,9 @@
 (ns flatten-array)
 
+(defn flatten-array [a]
+  (if (not (sequential? a))
+    (mapcat flatten-array a)
+    (list a)))
+
 (defn flatten [input]
-  (apply vector (remove nil? (clojure.core/flatten input))))
+  (apply vector (remove nil? (mapcat flatten-array input))))
