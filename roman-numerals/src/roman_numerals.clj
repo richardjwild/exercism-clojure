18eb58519deb19ff2efc08f9a40dd8f95efe74d3
Richard Wild <richard.wild@e2y.co.uk>
2017-09-22 08:25:59 +0100

    roman-numerals

---
+++ roman-numerals/src/roman_numerals.clj
@@ -0,0 +1,16 @@
+(ns roman-numerals)
+
+(def the-numerals
+  [["M" 1000]
+   ["CM" 900] ["D" 500] ["CD" 400] ["C" 100]
+   ["XC" 90] ["L" 50] ["XL" 40] ["X" 10]
+   ["IX" 9] ["V" 5] ["IV" 4] ["I" 1]])
+
+(defn numerals [number]
+  (loop [remaining number result "" index 0]
+    (if (zero? remaining)
+      result
+      (let [[numeral value] (the-numerals index)]
+        (if (>= remaining value)
+          (recur (- remaining value) (str result numeral) index)
+          (recur remaining result (inc index)))))))
