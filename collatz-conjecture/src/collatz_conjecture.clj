59337f54cad01c28b322f632dfc3b9e4d652ddef
Richard Wild <richard.wild@e2y.co.uk>
2017-09-25 07:12:59 +0100

    collatz-conjecture

---
+++ collatz-conjecture/src/collatz_conjecture.clj
@@ -0,0 +1,12 @@
+(ns collatz-conjecture)
+
+(defn get-next [value]
+  (if (zero? (rem value 2))
+    (/ value 2)
+    (+ (* value 3) 1)))
+
+(defn collatz [n]
+  (if (<= n 0)
+    (throw (IllegalArgumentException. "Number must be greater than zero"))
+    (loop [step 0, value n]
+      (if (= 1 value) step (recur (inc step) (get-next value))))))
