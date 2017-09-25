3544b61904c4cc1e9cfb537a5c67e32b88a8f614
Richard Wild <richardwild76@gmail.com>
2017-09-25 18:08:28 +0100

    series (complete)

--- series/src/series.clj
+++ series/src/series.clj
@@ -1,20 +1,12 @@
 (ns series
   (:require [clojure.string :as str]))
 
-(defn get-slice-from [string length start]
-  (let [end (+ start length)]
-    (loop [pos start, slice ""]
-      (if (>= pos end)
-        slice
-        (recur
-          (inc pos)
-          (str slice (subs string pos (inc pos))))))))
-
 (defn slices [string n]
-  (let [no-of-slices (inc (- (count string) n))]
-    (loop [slices [], start-at 0]
-      (if (>= start-at no-of-slices)
-        slices
-        (recur
-          (conj slices (get-slice-from string n start-at))
-          (inc start-at))))))
+  (if (zero? n)
+    [""]
+    (loop [slices [], s string]
+      (let [slice (reduce str (take n s))]
+        (if (< (count slice) n)
+          slices
+          (recur (conj slices slice) (reduce str (drop 1 s))))))))
+
