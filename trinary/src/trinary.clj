86fbc76121ff7d66a332015a51d9b293c0538c1e
Richard Wild <richardwild76@gmail.com>
2017-09-29 14:47:44 +0100

    trinary

---
+++ trinary/src/trinary.clj
@@ -0,0 +1,19 @@
+(ns trinary)
+
+(def base 3)
+
+(defn to-int [digit]
+  (- (int digit) (int \0)))
+
+(defn valid-digit? [digit]
+  (let [n (to-int digit)]
+    (and (>= n 0) (< n base))))
+
+(defn valid-number? [str]
+  (empty? (remove valid-digit? str)))
+
+(defn scale-then-add-digit [accumulator digit]
+  (+ (* accumulator base) (to-int digit)))
+
+(defn to-decimal [str]
+  (if (valid-number? str) (reduce scale-then-add-digit 0 str) 0))
