36918cafad8887b8dcf4753fd945f8d871653515
Richard Wild <richard.wild@e2y.co.uk>
2017-10-13 08:13:13 +0100

    sieve

---
+++ sieve/src/sieve.clj
@@ -0,0 +1,12 @@
+(ns sieve)
+
+(defn non-primes-up-to [limit]
+  (let [half-limit (-> (/ limit 2) Math/floor int)]
+    (loop [n 2, non-primes '()]
+      (if (> n half-limit)
+        (set non-primes)
+        (recur (inc n) (concat non-primes (range (* n 2) (inc limit) n)))))))
+
+(defn sieve [limit]
+  (let [non-primes (non-primes-up-to limit)]
+    (remove #(contains? non-primes %) (range 2 (inc limit)))))
