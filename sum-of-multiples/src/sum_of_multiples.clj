ceee8e5aa2674d63156508b0b36203edace9bb7c
Richard Wild <richard.wild@e2y.co.uk>
2017-10-13 08:13:07 +0100

    sum-of-multiples iteration 2

--- sum-of-multiples/src/sum_of_multiples.clj
+++ sum-of-multiples/src/sum_of_multiples.clj
@@ -1,10 +1,7 @@
 (ns sum-of-multiples)
 
 (defn multiples-up-to [limit of]
-  (loop [multiples [], multiple of]
-    (if (>= multiple limit)
-      multiples
-      (recur (conj multiples multiple) (+ multiple of)))))
+  (range of limit of))
 
 (defn sum-of-multiples [numbers limit]
   (reduce + (set (mapcat (partial multiples-up-to limit) numbers))))
