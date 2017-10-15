82b69953aa5a537da5cf2471df3bb6b16617b48b
Richard Wild <richardwild76@gmail.com>
2017-10-15 07:15:39 +0100

    sieve iteration 5

--- sieve/src/sieve.clj
+++ sieve/src/sieve.clj
@@ -1,12 +1,22 @@
-(ns sieve)
+(ns sieve
+  (require [clojure.set :as set]))
 
-(defn ^:private non-primes-up-to [limit]
-  (let [half-limit (-> (/ limit 2) Math/floor int)]
-    (loop [n 2, non-primes '()]
-      (if (> n half-limit)
-        (set non-primes)
-        (recur (inc n) (concat non-primes (range (* n 2) (inc limit) n)))))))
+(defn- multiples-up-to [limit n]
+  (range (* n 2) (inc limit) n))
+
+(defn- half [limit]
+  (-> (/ limit 2)
+      Math/ceil
+      int))
+
+(defn- non-primes-up-to [limit]
+  (-> (mapcat (partial multiples-up-to limit) (range 2 (half limit)))
+      set))
+
+(defn- numbers-up-to [limit]
+  (-> (range 2 (inc limit))
+      set))
 
 (defn sieve [limit]
-  (let [non-primes (non-primes-up-to limit)]
-    (remove #(contains? non-primes %) (range 2 (inc limit)))))
+  (-> (set/difference (numbers-up-to limit) (non-primes-up-to limit))
+      sort))
