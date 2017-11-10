8cda1e6e1214269c10bc278ddc3985647954849a
Richard Wild <richardwild76@gmail.com>
2017-11-10 08:08:20 +0000

    minesweeper iteration 3

--- minesweeper/src/minesweeper.clj
+++ minesweeper/src/minesweeper.clj
@@ -16,34 +16,32 @@
 (defn reduce-cells [& cells]
   (reduce + cells))
 
-(defn above-left [x y] [(dec x), (inc y)])
-(defn above [x y] [x, (inc y)])
-(defn above-right [x y] [(inc x), (inc y)])
-(defn left [x y] [(dec x), y])
-(defn right [x y] [(inc x), y])
-(defn below-left [x y] [(dec x), (dec y)])
-(defn below [x y] [x, (dec y)])
-(defn below-right [x y] [(inc x), (dec y)])
-
-(defn neighbours-of [x y]
-  ((juxt above-left above above-right left right below-left below below-right) x y))
-
-(defn neighbouring-cells [neighbours y x]
+(defn generate-cell [neighbours y x]
   (if (contains? neighbours [x y]) 1 0))
 
-(defn neighbouring-lines [neighbours width y]
-  (map (partial neighbouring-cells neighbours y) (take width (range))))
+(defn generate-line [neighbours width y]
+  (map (partial generate-cell neighbours y) (take width (range))))
 
-(defn generate-neighbours [width height neighbouring-cells]
-  (mapcat (partial neighbouring-lines neighbouring-cells width) (take height (range))))
+(defn generate-board [width height neighbouring-cells]
+  (mapcat (partial generate-line neighbouring-cells width) (take height (range))))
 
-(defn generate-neighbour-cells
+(def neighbours-of
+  (juxt (fn [x y] [(dec x), (inc y)])
+        (fn [x y] [x, (inc y)])
+        (fn [x y] [(inc x), (inc y)])
+        (fn [x y] [(dec x), y])
+        (fn [x y] [(inc x), y])
+        (fn [x y] [(dec x), (dec y)])
+        (fn [x y] [x, (dec y)])
+        (fn [x y] [(inc x), (dec y)])))
+
+(defn board-of-neighbours-for-cell
   ([width height line y]
-   (map (partial generate-neighbour-cells width height y) line (range)))
+   (map (partial board-of-neighbours-for-cell width height y) line (range)))
   ([width height y cell x]
    (if (mine? cell)
-     (generate-neighbours width height (set (neighbours-of x y)))
-     (generate-neighbours width height #{}))))
+     (generate-board width height (set (neighbours-of x y)))
+     (generate-board width height #{}))))
 
 (defn draw [board-string]
   (case board-string
@@ -51,7 +49,7 @@
     (let [board-lines (str/split-lines board-string),
           height (count board-lines),
           width (count (first board-lines))]
-      (->> (mapcat (partial generate-neighbour-cells width height) board-lines (range))
+      (->> (mapcat (partial board-of-neighbours-for-cell width height) board-lines (range))
            (apply map reduce-cells)
            (map number-to-text)
            (partition width)
